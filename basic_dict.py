6c456518bade1c6906fa113009a6f40d2b2d5e35
dlwlndjsald <dlwlndjsald@gmail.com>
2024-08-06 16:44:03 +0900

    0806 set, tuple, dict, func

--- basic_dict.py
+++ basic_dict.py
@@ -2,6 +2,7 @@
 def define_dict():
     """
     dict 사전의 정의_
+    :  dict는 키 목록(dict_keys)과 값 목록(dict_values)이 합쳐진 복합 자료형
     """
     # 타입 함수를 이용한 생성
     dct = dict() # 빈 dict
@@ -23,7 +24,129 @@ def define_dict():
     print("dct5:", dct, type(dct))
 
 
+def dict_oper():
+    """
+    dict의 연산
+    : 키를 통한 접근 , 가변 자료형
+        len, in, not in,
+        기본적인 연산 대상은 keySet
+    """
+
+    dct = {"baseball":9, "basketball":5}
+
+    # 키를 통한 접근
+    dct['soccer'] = 11
+    print("dct1:", dct, type(dct))
+
+    # 순서가 없음: 인덱싱, 슬라이싱 불가
+    # 길이, 포함 여부 확인 가능
+    print("dct2:", dct, "LENGTH:", len(dct))
+
+    print("baseball" in dct)
+    print("soccer" in dct)
+    print("volleyball" in dct)
+
+    # 포함 여부를 값 목록에서 확인할 경우: values()
+    print(9 in dct.values())
+
+    # .keys() : key set를 받아올 수 있음
+    # .values() : value 목록을 받아올 수 있음
+    # .items() : (키,값) 목록을 받아올 수 있음
+
+    # dict의 키 : 해싱되야 하므로 immutable 자료여야 함
+    dct[True] = "true"
+    dct[10] = 10
+    dct["eleven"] = 11
+    dct[(1,2,3)] = 6
+
+    dct[[1, 2, 3]] = 6 #TypeError: unhashable type: 'list'
+
+
+def dict_methods():
+    """
+    dict의 메서드들
+    """
+    dct = {"baseball":9, "basketball":5, "soccer":11}
+    print("dct:", dct, type(dct))
+
+    #set(key)-List(value)합쳐진 복합 자료형
+    keys = dct.keys()
+    print("keys:", keys, type(keys)) #dict_key
+
+    # dict_key도 순차자료형 : set, list 타입 함수로 변환 후 순차형 사용가능
+    lst_keys = list(keys)
+    print("lst_keys:", lst_keys, type(lst_keys))
+
+    # 값의 목록 : .values() # dict_values
+    values = dct.values()
+    print("values:", values, type(values))
+
+    #(키, 값) 튜플의 목록: .items() # dict_items
+    items = dct.items()
+    print("items:", items, type(items))
+
+    lst_items = list(items)
+    print("key:", lst_items[1][0], ", value:", lst_items[1][1])
+
+    # dict 비우기
+    dct.clear()
+    print(dct)
+
+
+def using_dict():
+    """
+    dict 사용법
+    """
+    phones = {
+        "둘리": "010-1234-5678",
+        "도우너": "010-9876-5432",
+        "또치": "010-2222-3333"
+    }
+    print("phones:", phones)
+
+    #키 접근 vs get() 메서드
+    print("둘리 전화번호:", phones["둘리"])
+    # print("고길동 전화번호:", phones["고길동"]) #없는 키에 접근시 KeyError
+    print("고길동 전화번호:", phones.get("고길동", "고길동키로get했지만몰라로출력해"))
+
+    #아이템 삭제
+    del phones['또치']
+
+    # 아이템을 가져오고 동시에 삭제 : pop
+    item = phones.pop("도우너")
+    print("pop_item:", item)
+    print("phones:", phones)
+
+
+def loop():
+    """
+    사전의 순회(loop) : 기본적으로 keySet 대상
+    """
+
+    phones = {
+        "둘리": "010-1234-5678",
+        "도우너": "010-9876-5432",
+        "또치": "010-2222-3333"
+    }
+
+    # 기본적인 루프
+    for key in phones:
+        print(f"{key}:{phones.get(key)}")
+
+    for key in phones.keys(): #위와 동일
+        print(f"{key}:{phones.get(key)}")
+
+    print("items:", phones.items())
+    for key,value in phones.items(): # (키,값) ->unpacking
+        print(f"{key}:{value}")
+
+
+
 
 
 if __name__ == "__main__":
-    define_dict()
+    #define_dict()
+    #dict_oper()
+    #dict_methods()
+    #using_dict()
+    loop()
---
+++ basic_seq_func.py
@@ -0,0 +1,105 @@
+
+def using_range():
+    """
+    range 객체 :범위 객체
+    - 범위 정보만 가지고 있다가 요청할 때 조건의 계산한 후 결과를 반환
+    - 큰 범위 데이터를 생성해도 메모리가 증가하지 않음 : generator
+    :syntax: range([start=0, ] end [, stop=1])
+    """
+    #인자값이 1개 -> 끝값
+    seq = range(10) #10은 범위에 포함되지 않음
+    print("seq:", seq, type(seq))
+    print("seq_list:", list(seq)) #확인용// 굳이 리스트화할필요X
+
+    #인자값이 2개 -> 시작값, 끝값
+    seq2 = range(2,10) #2이상, 10미만
+    print("seq2:", seq2)
+    print("seq2_list:", list(seq2))
+
+    #인자값이 3개 -> 시작값, 끝값, 간격값
+    seq3 = range(0, 10, 2) #2이상, 10미만, 2간격
+    print("seq3:", seq3)
+    print("seq3_list:", list(seq3))
+
+    # 큰수 -> 작은수 : 간격값을 음수로
+    seq4 = range(0, -10, -1) #0이하, -10초과, -1간격(역순출력)
+    print("seq4:", seq4)
+    print("seq4_list:", list(seq4))
+
+    # range도 순차 자료형 : len, 인덱싱, 슬라이싱, in, not in 가능
+    print("seq:", seq, "LENGTH:", len(seq))
+    print("5 in seq:", 5 in seq) #포함여부 확인
+    print(seq[-3], seq[-2], seq[-1], #역방향 인덱스
+          seq[0], seq[1], seq[2], seq[3]) #정방향 인덱스
+
+    # range는 변경 불가
+    # seq[0] = 10 # 슬라이싱 가능, 치환은X
+
+    # loop
+    # ex) 1-10까지 2간격 루프돌릴때
+    for i in range(1,11,2):
+        print(i,end="\t")
+    else:
+        print()
+
+    # 로또
+    import itertools
+    it = itertools.combinations(range(1, 46), 6)
+    for num in it:
+        print(num)
+
+
+def using_enumerate():
+    """
+    enumerate 함수
+    : 순차 자료형을 인덱스와 함께 튜플로 출력해주는 함수: generator
+    : 순차 자료형에서 아이템과 함께 아이템의 인덱스를 함꼐 활용하고자 할 때 활용
+    """
+
+    # 루프를 돌리면서 인덱스와 함께 출력하고자 할때
+    colors = ["red", "yellow", "blue", "green", "white", "black", "gray"]
+    i = 0  # index위한
+    for color in colors:
+        print("COLOR {0}:{1}".format(i,color))
+        i += 1
+
+    print("=====enumerate 1=====")
+    for index, color in enumerate(colors):  # (인덱스정보, 요소값)
+        print(f"COLOR {index}:{color}")
+
+    # 다른 방법 : enumerate 함수를 사용했을 때
+    print("=====enumerate 2=====")
+    for i, color in enumerate(colors):
+        print("COLOR {0}:{1}".format(i,color))
+
+
+def using_zip():
+    """
+    zip: 여러 시퀀스 자료를 동시에 루프시켜 튜플로 생성해주는 Generator
+    """
+    eng = ["Sunday", "Monday", "Tuesday", "Wednesday"]
+    kor = ["일요일", "월요일", "화요일", "수요일", "목요일"]
+
+    engkor = zip(eng, kor)
+    print(engkor, type(engkor))
+
+    # 기본 순회
+    for pair in engkor:
+        #print("pair:", pair, type(pair))
+        print(f"영어 {pair[0]} -> 한국어 {pair[1]}")
+
+    print("=============unpacking======")
+    engkor = zip(eng,kor)
+    #unpacking
+    for eng, kor in engkor:
+        print(f"영어 {eng} -> 한국어 {kor}")
+
+
+
+
+
+
+if __name__ == "__main__":
+    #using_range()
+    #using_enumerate()
+    using_zip()
